1bc66786764e86bc0b8d0d16484eef1c5ccd3267
Felipe Affonso <felipe.affonso@mths.ca>
2021-04-23 14:37:55 +0000

    Hello World

---
+++ helloWorld.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Felipe Affonso All rights reserved
+//
+// Created by: Felipe Affonso
+// Created on: april 2021
+// This is the "Hello, World!" program, with proper style
+
+#include <iostream>
+
+int main()    {
+    std::cout << "Hello, World!" << std::endl;
+}
